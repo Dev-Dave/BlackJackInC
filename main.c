2d503f07aeaa8299dcbd741424b35dc9090274dd
Dev-Dave <Dev-Dave@users.noreply.github.com>
2015-03-15 13:35:15 -0400

    Added a.out and added changes to main.c

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
-/* main.c Black Jack Program */
+/* 
+
+File: main.c 
+Title: Black Jack Program 
+By: Dev-Dave
+Date Created: March 15, 2015
+Purpose: A Test of GitHub.com features
+
+*/
 #include <stdio.h>
 
 int main(void) {
-	
+	printf("Black Jack in C\nby: Dev-Dave\ncreated on: March 15, 2015\n");
 	return(0);
 }
 
