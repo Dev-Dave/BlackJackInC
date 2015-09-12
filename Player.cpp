bf6b06c3f77abdc8534bd7b5cf31699c1419e2eb
David Caouette <dc1014@turing.plymouth.edu>
2015-09-11 21:54:55 -0400

    Add player number value and getNum() function to Player.cpp
    - Add playerNum variable to Player() class
    - Add getNum() function to Player() class

--- Player.cpp
+++ Player.cpp
@@ -8,15 +8,19 @@ class Player {
 	
 	string playerName;
 	int playerAge;
+	int playerNum;
+
 	public:
-		Player(string, int);
+		Player(string, int, int);
 		string getName();
 		int getAge();
+		int getNum();
 };
 
-Player::Player(string name, int age) {
+Player::Player(string name, int age, int num) {
 	playerName = name;
 	playerAge = age;
+	playerNum = num;
 }
 
 string Player::getName() {
@@ -27,10 +31,15 @@ int Player::getAge() {
 	return playerAge;
 }
 
+int Player::getNum() {
+	return playerNum;
+}
+
 int main() {
-	Player player ("Dave", 23);
+	Player player ("Dave", 23, 1);
 	cout << "Player: " << endl;
 	cout << "Name: " << player.getName() << endl;
 	cout << "Age: " << player.getAge() << endl;
+	cout << "Number: " << player.getNum() << endl;
 	return 0;
 }
