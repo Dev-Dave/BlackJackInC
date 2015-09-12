312b9fefa6182ab5c19550ab02505492ccf6dce2
David Caouette <dc1014@turing.plymouth.edu>
2015-09-11 20:21:00 -0400

    Add age value and getAge() method to Player class.
    - Add age field to track player age
    - Add getAge() method for returning player age

--- Player.cpp
+++ Player.cpp
@@ -7,22 +7,30 @@ using namespace std;
 class Player {
 	
 	string playerName;
-
+	int playerAge;
 	public:
-		Player(string);
+		Player(string, int);
 		string getName();
+		int getAge();
 };
 
-Player::Player(string name) {
+Player::Player(string name, int age) {
 	playerName = name;
+	playerAge = age;
 }
 
 string Player::getName() {
 	return playerName;
 }
 
+int Player::getAge() {
+	return playerAge;
+}
+
 int main() {
-	Player player ("Dave");
-	cout << player.getName() << endl;
+	Player player ("Dave", 23);
+	cout << "Player: " << endl;
+	cout << "Name: " << player.getName() << endl;
+	cout << "Age: " << player.getAge() << endl;
 	return 0;
 }
